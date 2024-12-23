61079c5b7acc49b78635aafaa8a116d7d0f64a8b
Furkan Aktas <f_rkan_aktas@proton.me>
2024-12-23 19:41:05 +0100

    Exercise 2 Task 1

--- exercise2/nand.h
+++ exercise2/nand.h
@@ -6,4 +6,20 @@
 
 // TODO: Insert your code here
 
+SC_MODULE(nand){
+public:
+    sc_in<bool> A;
+    sc_in<bool> B;
+    sc_out<bool> Z;
+
+    SC_CTOR(nand) : A("A"), B("B") ,Z("Z"){
+        SC_METHOD(process);
+        sensitive << A << B << Z;
+    }
+
+    void process(){
+        Z.write(!(A.read() && B.read())); 
+    }
+};
+
 #endif
